b5e59bb02b3f170bc0f2c08215a752df550b0314
Andrew Boring <andrew@andrewboring.com>
2018-09-26 13:31:10 -0400

    Added build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@ for i in `ls -d */`; do (cd $i && makepkg -sr); done
 mkdir carbon
 find . -name \*armv7h\*xz -exec mv -t ./carbon {} +
 (cd carbon && repo-add carbon.db.tar.gz *armv7h*)
+
+
+echo "Don't forget to rsync the carbon folder to /beta/armv7h/"
+echo ""
+
